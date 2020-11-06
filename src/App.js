709b362beaf95183120ad7b7ae9711dec47a30d3
sevinn21 <sevinn20@gmail.com>
2020-11-06 22:21:54 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import './Burger.css'
+import { Route, Router, Switch } from 'react-router-dom';
+import Header from './Screen/Header';
+import Counter from './Component/Counter';
 
 const App = () => {
-  return <p>Baslangıç Noktası</p>;
+  return  (
+    <>
+    <div className="demo-header">
+    <Header/>
+    </div>
+    <Counter />
+     </>
+  ) 
+ 
 };
 
 export default App;
