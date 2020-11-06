cfa3cdfebe2ecf4f28d501345d08a7257a215851
sevinn21 <sevinn20@gmail.com>
2020-11-06 22:20:05 +0300

    Create Button

---
+++ src/Component/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({ color, text, ...restProps }) => {
+    return (
+      <button style={{ color: color, width:"50px",height:"30px" }} {...restProps}>
+        {text}
+      </button>
+    );
+  };
+
+export default Button
