5fe539a15b71f35eda5c8fb62df98fccf7f05d92
sevinn21 <sevinn20@gmail.com>
2020-11-06 22:31:11 +0300

    create-header

---
+++ src/Screen/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="demo-header">
+           <button >Orders</button>
+        </div>
+    )
+}
+
+export default Header
